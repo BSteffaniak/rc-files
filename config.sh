9836b0071ff5870dc6be268b57a5c94f84337b05
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-02-01 22:12:32 -0500

    Add tmux-bash-history config

--- config.sh
+++ config.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# tmux-bash-history
+if [[ -n "$TMUX" ]]; then
+    # Optionally set a custom bash history home directory:
+    # tmux set -g @bash-history-home "~/path/to/custom/dir"
+    [ -f "$HOME/.config/tmux/plugins/tmux-bash-history/scripts/restore_history.sh" ] && . "$HOME/.config/tmux/plugins/tmux-bash-history/scripts/restore_history.sh"
+fi
+
 shopt -u progcomp
 
 PS1="\[\033[01;33m\]\w\[\033[00m\] \$ "
